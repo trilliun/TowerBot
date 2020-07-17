296547c14c817c070cdbcee79f527ab98efc0c11
Sherry <trilliun@users.noreply.github.com>
2020-07-16 20:46:47 -0400

    add main files

---
+++ bot.js
@@ -0,0 +1,42 @@
+//declare using statements and variables
+const Discord = require("discord.js");
+const fs = require("fs");
+const mongoose = require('mongoose');
+const config = require("./config.json");
+
+const client = new Discord.Client();
+client.config = config;
+
+//load up listeners and events
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+//load up commands
+client.commands = new Discord.Collection();
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+  });
+});
+console.log(`Loaded commands`);
+
+//connect to database
+const db = mongoose
+  .connect(config.mongoUri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connnection successful!'));  
+
+//log in
+client.login(config.token);
---
+++ events/message.js
@@ -0,0 +1,37 @@
+module.exports = (client, message) => {
+  // Ignore all bots
+  if (message.author.bot) return;
+
+  // Ignore messages not starting with the prefix (in config.json)
+  if (message.content.indexOf(client.config.prefix) !== 0) return;
+
+  // Our standard argument/command name definition.
+  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+  const commandName = args.shift().toLowerCase();
+
+  // Grab the command data from the client.commands Enmap
+  const command = client.commands.get(commandName) ||
+    client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+  // If that command doesn't exist, silently exit and do nothing
+  if (!command) return;
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${config.prefix}${command.name} ${command.usage}\``;
+    }
+
+    return message.channel.send(reply);
+  }
+
+  try {
+    console.log(`running command ${command.name}`);
+    command.execute(client, message, args);
+  }
+  catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute your command!');
+  }
+};
