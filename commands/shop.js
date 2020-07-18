db7df5385b19f09cb52b1b6c8ddf8996867d5ed8
Sherry <trilliun@users.noreply.github.com>
2020-07-18 12:49:42 -0400

    add pagination to shop page

--- commands/shop.js
+++ commands/shop.js
@@ -2,21 +2,26 @@ const Discord = require('discord.js');
 const Item = require('../models/item.js');
 const utility = require('../utility.js');
 const config = require('../config.json');
+const paginationEmbed = require('discord.js-pagination');
+
+const createEmbed = (message) => {
+    const description = `Use command \`${config.prefix} buy [quantity] <item-name (or shortcode)>\` to purchase an item from the shop.\n${'༞'.padEnd(35, '༞')}`;
+    const color = '#DA70D6';
+
+    return new Discord.MessageEmbed()
+        .setColor(color)
+        .setAuthor(`${message.guild.name} Shop`, message.guild.iconURL())
+        .setDescription(description);
+}
 
 module.exports = {
     name: 'shop',
     description: 'displays a list of items available in the club shop',
     async execute(client, message, args) {
-
-        //create embed
-        const shopEmbed = new Discord.MessageEmbed();
-        shopEmbed
-            .setColor('#DA70D6')
-            .setAuthor(`${message.guild.name} Shop`, message.guild.iconURL())
-            .setDescription(`Use command \`${config.prefix} buy [quantity] <item-name>\` to purchase an item from the shop.\n${'༞'.padEnd(35, '༞')}`);
+        const pages = [];
 
         //get items from database
-        Item.find().exec((err, itemsQuery) => {
+        Item.find().exec(async (err, itemsQuery) => {
             let itemGroups = new Map();
 
             itemsQuery.forEach(i => {
@@ -30,23 +35,55 @@ module.exports = {
                 }
             });
 
-            itemGroups.forEach((value, key, map) => {
-                let cat = key.toUpperCase();
-                let catItemStr = [];
-                value.forEach(catItem => {
-                    var itemIcon = utility.getEmoji(catItem.icon, client);
+            //begin constructing pages
+            //for each category
+            itemGroups.forEach((itemGroup, category, map) => {
+                //create embed
+                let embed = createEmbed(message);
+
+                var cat = category.toUpperCase();
+                let itemStr = [];
+
+                //for each item in the category
+                itemGroup.forEach(groupItem => {
+                    var itemIcon = utility.getEmoji(groupItem.icon, client);
                     var currencyIcon = utility.getEmoji('tbbabelgold', client);
-                    if (catItem.description) {
-                        var desc = catItem.description;
+                    if (groupItem.description) {
+                        var desc = groupItem.description;
                     } else {
                         var desc = 'no description';
                     }
-                    catItemStr.push(`\n${itemIcon} \`${catItem.name.padEnd(15, ' ')}\` ${currencyIcon} **${catItem.cost}**\n*${desc}*\n`)
+
+                    itemStr.push(`${itemIcon} \`${groupItem.name.padEnd(15, ' ')}\` ${currencyIcon} **${groupItem.cost}**\n*${desc}*\n`);
+
+                    if (itemStr.length == 5 || itemGroup.indexOf(groupItem) + 1 == itemGroup.length) {
+                        if (itemGroup.indexOf(groupItem) > 5) {
+                            embed.addField(cat += ' CONTD.', itemStr, true);
+                        } else {
+                            embed.addField(cat, itemStr, true);
+                        }
+                        itemStr = [];
+                    } //limit items to 10 max per page
+                    if (itemGroup.indexOf(groupItem) + 1 == 10) {
+                        embed = createEmbed();
+                    }
                 });
-                shopEmbed.addField(cat, catItemStr.join(''), true);
+                pages.push(embed);
             });
 
-            message.channel.send(shopEmbed);
+            pages.forEach(page => {
+                page.setFooter(`${pages.indexOf(page) + 1}/${pages.length} pages`);
+            });
+
+            let pIndex = 0;
+            if (args.length != 0) {
+                //ensure arg is a number within range
+                if (Number(args[0]) >= 0 && Number(args[0]) <= pages.length) {
+                    pIndex = Number(args[0]) - 1;
+                }
+            }
+
+            paginationEmbed(message, pages);           
         });
     }
 }
