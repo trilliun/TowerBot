446b13bcf4eeba59065092be3dbd86388ba28199
Sherry <trilliun@users.noreply.github.com>
2020-07-20 21:17:44 -0400

    remove unused code, make adjustments to how shop pages are constructed

--- commands/shop.js
+++ commands/shop.js
@@ -1,26 +1,33 @@
 const Discord = require('discord.js');
 const Item = require('../models/item.js');
+const User = require('../models/user.js');
 const utility = require('../utility.js');
 const config = require('../config.json');
 const paginationEmbed = require('discord.js-pagination');
 
 const createEmbed = (message) => {
-    const description = `Use command \`${config.prefix} buy [quantity] <item-name>\` to purchase an item from the shop.\n${'༞'.padEnd(35, '༞')}`;
     const color = '#DA70D6';
 
     return new Discord.MessageEmbed()
         .setColor(color)
-        .setAuthor(`${message.guild.name} Shop`, message.guild.iconURL())
-        .setDescription(description);
+        .setAuthor(`${message.guild.name} Shop`, message.guild.iconURL());
 }
 
 module.exports = {
     name: 'shop',
-    description: 'displays a list of items available in the club shop',
+    description: 'displays a list of items available in the general shop',
     async execute(client, message, args) {
         const pages = [];
+        const user = await User.findOne({'userId': message.author.id});
+        if (user == null || user == undefined || user.currency == 0) {
+            var spend = '**no money**';
+        } else {
+            var spend = utility.getCurrencyString(user.currency, client);
+        }
+        
+        var shopDescription = `Use command \`${config.prefix} buy [quantity] <item-name>\` to purchase an item from the shop.\nYou currently has ${spend} to spend.\n${'༞'.padEnd(35, '༞')}`;
 
-        //get items from database
+        //get common items from database
         Item.find().exec(async (err, itemsQuery) => {
             let itemGroups = new Map();
 
@@ -40,6 +47,7 @@ module.exports = {
             itemGroups.forEach((itemGroup, category, map) => {
                 //create embed
                 let embed = createEmbed(message);
+                embed.setDescription(shopDescription);
 
                 var cat = category.toUpperCase();
                 let itemStr = [];
@@ -47,14 +55,14 @@ module.exports = {
                 //for each item in the category
                 itemGroup.forEach(groupItem => {
                     var itemIcon = utility.getEmoji(groupItem.icon, client);
-                    var currencyIcon = utility.getEmoji('tbbabelgold', client);
+                    let currencyString = utility.getCurrencyString(groupItem.cost, client);
                     if (groupItem.description) {
                         var desc = groupItem.description;
                     } else {
                         var desc = 'no description';
                     }
 
-                    itemStr.push(`${itemIcon} \`${groupItem.name.padEnd(15, ' ')}\` ${currencyIcon} **${groupItem.cost}**\n*${desc}*\n`);
+                    itemStr.push(`${itemIcon} \`${groupItem.name.padEnd(15, ' ')}\` ${currencyString}\n*${desc}*\n`);
 
                     if (itemStr.length == 5 || itemGroup.indexOf(groupItem) + 1 == itemGroup.length) {
                         if (itemGroup.indexOf(groupItem) > 5) {
@@ -75,15 +83,11 @@ module.exports = {
                 page.setFooter(`${pages.indexOf(page) + 1}/${pages.length} pages`);
             });
 
-            let pIndex = 0;
-            if (args.length != 0) {
-                //ensure arg is a number within range
-                if (Number(args[0]) >= 0 && Number(args[0]) <= pages.length) {
-                    pIndex = Number(args[0]) - 1;
-                }
+            if (pages.length > 1) {
+                paginationEmbed(message, pages);           
+            } else {
+                message.channel.send(pages[0]);
             }
-
-            paginationEmbed(message, pages);           
         });
     }
 }
