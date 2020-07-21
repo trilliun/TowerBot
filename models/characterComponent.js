3d73bd90483bb49eea451ad945d6f101adbd59e4
Sherry <trilliun@users.noreply.github.com>
2020-07-21 19:35:52 -0400

    Create characterComponent.js

---
+++ models/characterComponent.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const characterComponentSchema = mongoose.Schema({
+    component: String,
+    description: String,
+    cost: Number,
+    uri: []
+});
+
+module.exports = mongoose.model('CharacterComponent', characterComponentSchema);
