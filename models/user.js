df2d742071601138a2d92a917f6d512f92b55c61
Sherry <trilliun@users.noreply.github.com>
2020-07-20 21:18:19 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     userId: String,
-    currency: Number,
+    currency: {type: Number, default: 0},
+    level: {type: Number, default: 1},
+    femaleCharacter: {type: Boolean, default: true},
+    appearanceType: {type: Number, default:0},
+    profileBackground: { type: mongoose.Types.ObjectId, default: mongoose.Types.ObjectId('5f139709d543d0264484eb78')},
+    profilePicture: { type: mongoose.Types.ObjectId, default: mongoose.Types.ObjectId('5f13b538d543d0264484eb91')},
     inventory: [{
         item: mongoose.Types.ObjectId,
         quantity: Number
