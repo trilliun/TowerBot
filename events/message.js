02c6bd9cdbdc5dbda29f759e4c999edfe67addfa
Sherry <trilliun@users.noreply.github.com>
2020-07-21 10:17:41 -0400

    remove case sensitivity on prefix

--- events/message.js
+++ events/message.js
@@ -3,7 +3,7 @@ module.exports = (client, message) => {
   if (message.author.bot) return;
 
   // Ignore messages not starting with the prefix (in config.json)
-  if (message.content.indexOf(client.config.prefix) !== 0) return;
+  if (message.content.indexOf(client.config.prefix.toLowerCase()) !== 0) return;
 
   // Our standard argument/command name definition.
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
