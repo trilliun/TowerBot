5e7bfa44e9164d3ca9c74345ed1a807115b7489a
Sherry <trilliun@users.noreply.github.com>
2020-07-22 13:33:44 -0400

    Update item.js

--- models/item.js
+++ models/item.js
@@ -3,8 +3,16 @@ const mongoose = require('mongoose');
 const itemSchema = mongoose.Schema({
     name: String,
     description: String,
+    details: String,
     cost: Number,
-    icon: String
+    category: String,
+    rarity: String,
+    icon: String,
+    roll: {
+        dieCount: Number,
+        dieType: String,
+        modifier: Number
+    }
 });
 
 module.exports = mongoose.model('Item', itemSchema);
