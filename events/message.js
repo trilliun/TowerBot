3b93284622f369c8d138f9e0fd0232c0a192c902
Sherry <trilliun@users.noreply.github.com>
2020-07-22 13:33:54 -0400

    add check for cooldowns

--- events/message.js
+++ events/message.js
@@ -1,6 +1,8 @@
 const utility = require('../utility.js');
 const User = require('../models/user.js');
 const PlayerLevel = require('../models/playerLevel.js');
+const Discord = require('discord.js');
+const cooldowns = new Discord.Collection();
 let user = null;
 
 module.exports = async (client, message) => {
@@ -30,6 +32,25 @@ module.exports = async (client, message) => {
 
     return message.channel.send(reply);
   }
+
+  //check for cooldown
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+    }
+  }
+
   try {
     if (user == null || user == undefined) {
       //find or create user profile
